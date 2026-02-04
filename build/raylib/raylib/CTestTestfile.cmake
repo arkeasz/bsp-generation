53fdf9fb16b00aaac394fc19e401b7898ca2285c
arki <yoarkirus@gmail.com>
2026-02-03 23:42:26 -0500

    bye bye build

--- build/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arki/projects/bsp-trees/raylib/src
-# Build directory: /home/arki/projects/bsp-trees/build/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
